58821a9ed2e8575fe389cf9f02daa562e14c9833
xuanlan <guoliangxuanlan@outlook.com>
2019-05-13 13:33:36 +0800

    Optimized.

--- cgi-bin/addr.c
+++ cgi-bin/addr.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[], char *envp[]){
-    fprintf(stdout, "<h1>%d + %d = %d</h1>", atoi(argv[1]), atoi(argv[2]), atoi(argv[1]) + atoi(argv[2]));
+    fprintf(stdout, "<h1>%d + %d = %d</h1>", atoi(argv[1]), atoi(argv[2]), 
+    atoi(argv[1]) + atoi(argv[2]));
     return 0;
 }
